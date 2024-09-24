5ad25926539ab58c1b9efd273e302dd3f8d11e0d
Alex S <insten490@gmail.com>
2024-09-23 23:23:17 -0500

    initial template for linkedList

---
+++ src/linkedList.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        pass
+
+    def insertAtBegin(self, val):
+        pass
+
+    def insertAtEnd(self, val):
+        pass
+
+    def insertAtIndex(self, idx, val):
+        pass
+
+    def removeNode(self, val):
+        pass
+
+    def printAll(self):
+        pass
+
+    def sizeOfAll(self):
+        pass
+
+if __name__ == '__main__':
+    pass
