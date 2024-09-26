95104f2519d95ce774b6e666946627a92077b4f5
Alex S <insten490@gmail.com>
2024-09-26 09:49:25 -0500

    added more methods

--- src/linkedList.py
+++ src/linkedList.py
@@ -17,7 +17,15 @@ class LinkedList:
             self.head = newNode
 
     def insertAtEnd(self, val):
-        pass
+        newNode = Node(val)
+        if self.head is None:
+            self.head = newNode
+            return
+
+        currentNode = self.head
+        while currentNode.next is not None:
+            currentNode = currentNode.next
+        currentNode.next = newNode
 
     def insertAtIndex(self, val, idx):
         if idx == 0:
@@ -34,19 +42,50 @@ class LinkedList:
             newNode.next = curNode.next
             curNode.next = newNode
         else:
-            raise Exception("Index not found")
-            
+            raise Exception(f"Index {idx} not found")
 
+    def updateNode(self, val, idx):
+        curNode = self.head
+        pos = 0
+        if pos == idx:
+            curNode.val = val
+        else:
+            while curNode != None and pos != idx:
+                curNode = curNode.next
+                pos += 1
+            if curNode != None:
+                curNode.val = val
+            else:
+                raise Exception(f"{val} at {idx} not found")
 
+    def removeFirstNode(self):
+        if self.head is None:
+            return
+        self.head = self.head.next
 
-    def removeNode(self, val):
-        pass
+    def removeLastNode(self):
+        if self.head is None:
+            return
+        curNode = self.head
+        while curNode != None and curNode.next.next != None:
+            curNode = curNode.next
+
+        curNode.next = None
+        
 
     def printAll(self):
         pass
 
-    def sizeOfAll(self):
-        pass
+    def sizeOfLL(self):
+        size = 0
+        if self.head:
+            curNode = self.head
+            while curNode != None:
+                size += 1
+                curNode = curNode.next
+            return size
+        else:
+            return 0
 
 if __name__ == '__main__':
     pass
