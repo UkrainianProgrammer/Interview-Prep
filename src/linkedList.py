184df52e454f6d9007c714ecf419550caeff1e4f
Alex S <insten490@gmail.com>
2024-09-26 21:52:21 -0500

    added more methods

--- src/linkedList.py
+++ src/linkedList.py
@@ -66,15 +66,33 @@ class LinkedList:
     def removeLastNode(self):
         if self.head is None:
             return
-        curNode = self.head
+
         while curNode != None and curNode.next.next != None:
             curNode = curNode.next
 
         curNode.next = None
+    
+    def removeNode(self, val):
+        if self.head is None:
+            return
         
+        curNode = self.head
+        while curNode != None and curNode.next.val != val:
+            curNode = curNode.next
+        
+        if curNode == None:
+            return
+        else:
+            curNode.next = curNode.next.next
 
     def printAll(self):
-        pass
+        if self.head:
+            curNode = self.head
+            while curNode:
+                print(curNode.val)
+                curNode = curNode.next
+        else:
+            raise Exception("Empty list")
 
     def sizeOfLL(self):
         size = 0
@@ -88,4 +106,35 @@ class LinkedList:
             return 0
 
 if __name__ == '__main__':
-    pass
+    # create a new linked list
+    llist = LinkedList()
+
+    # add nodes to the linked list
+    llist.insertAtEnd('a')
+    llist.insertAtEnd('b')
+    llist.insertAtBegin('c')
+    llist.insertAtEnd('d')
+    # llist.insertAtIndex('g', 2)
+
+    # print the linked list
+    print("Node Data")
+    llist.printLL()
+
+    # remove a nodes from the linked list
+    print("\nRemove First Node")
+    llist.remove_first_node()
+    print("Remove Last Node")
+    llist.remove_last_node()
+    print("Remove Node at Index 1")
+    llist.remove_at_index(1)
+
+    # print the linked list again
+    print("\nLinked list after removing a node:")
+    llist.printLL()
+
+    print("\nUpdate node Value")
+    llist.updateNode('z', 0)
+    llist.printLL()
+
+    print("\nSize of linked list :", end=" ")
+    print(llist.sizeOfLL())
