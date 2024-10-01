a59439006772c3cc83de043146959c32324742a0
Alex S <insten490@gmail.com>
2024-09-30 22:35:22 -0500

    Added the test code and finished basic methods for Tree

--- src/bst.py
+++ src/bst.py
@@ -1,41 +1,74 @@
-# Python implementation of Binary Search Tree
+# Python implementation of Binary Search Tree 
 
 class Node:
-    def __init__(self) -> None:
-        self.left = None
-        self.right = None
-        self.val = None
+    def __init__(self, val):
+        self.l = None
+        self.r = None
+        self.v = val
 
 class Tree:
-    def __init__(self) -> None:
+    def __init__(self):
         self.root = None
-    
-    def getRoot(self):
+
+    def get_root(self):
         return self.root
-    
-    def addNode(self, val):
+
+    def add(self, val):
         if not self.root:
             self.root = Node(val)
         else:
             self._add(val, self.root)
-    
+
     def _add(self, val, node):
-        if val < node.val:
-            if node.left:
-                self._add(val, node.left)
+        if val < node.v:
+            if node.l:
+                self._add(val, node.l)
             else:
-                node.left = Node(val)
+                node.l = Node(val)
         else:
-            if node.right:
-                self.add(val, node.right)
+            if node.r:
+                self._add(val, node.r)
             else:
-                node.right = Node(val)
-    
+                node.r = Node(val)
+
     def find(self, val):
         if self.root:
             return self._find(val, self.root)
-        else:
-            raise Exception(f'{val} does not exist in an empty Tree')
-    
-    def _find(self, val):
-        pass
+
+    def _find(self, val, node):
+        if val == node.v:
+            return node
+        elif val < node.v and node.l:
+            return self._find(val, node.l)
+        elif val > node.v and node.r:
+            return self._find(val, node.r)
+
+    def delete_tree(self):
+        # garbage collector will do this for us.
+        if self.root:
+            self.root = None
+
+    def view_tree(self):
+        if self.root:
+            self._view_tree(self.root)
+
+    def _view_tree(self, node):
+        if node:
+            self._view_tree(node.l)
+            print(node.v, end = " ")
+            self._view_tree(node.r)
+
+#     3
+# 0     4
+#   2      8
+tree = Tree()
+tree.add(3)
+tree.add(4)
+tree.add(0)
+tree.add(8)
+tree.add(2)
+tree.view_tree()
+print(tree.find(3).v)
+print(tree.find(10))
+tree.delete_tree()
+tree.view_tree()
